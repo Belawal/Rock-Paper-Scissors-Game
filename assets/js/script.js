3d2383533c956ef6db50616fdeb4ea39244439ac
Belawal Khan <Belawal.khan@gmail.com>
2024-09-08 20:46:59 +0000

    event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//wait for the Dom to be fully loaded before running the script
+document.addEventListener('DOMContent Loaded', function() {
+    
+    const choices = document.getElementsByClassName('choice');//choices from button
+    const resutDisplay = documnet.getElementsBy
+}
